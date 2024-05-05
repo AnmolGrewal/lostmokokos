5d8b3ad24f22e87963d756a70228ff2ead17079d
anmolgrewal <anmolsgrewal@gmail.com>
2024-05-05 14:23:24 -0700

    Adding Link Support

--- src/app/components/NavigationBar.tsx
+++ src/app/components/NavigationBar.tsx
@@ -17,16 +17,18 @@ const NavigationBar = ({ currentPath }: NavigationBarProps) => {
     <div className="bg-secondary-background-color w-full h-12 flex justify-center items-center">
       <div className="flex gap-4">
         {navItems.map(item => (
-          <Link href={item.path} key={item.label} passHref>
-            <a className={clsx(
+          (<Link
+            href={item.path}
+            key={item.label}
+            passHref
+            className={clsx(
               "flex items-center px-4 py-2 rounded-full transition-all duration-300 ease-in-out cursor-pointer",
               currentPath === item.path ? 'bg-blue-500' : 'bg-[#282B2F]',
               "hover:bg-blue-600 hover:scale-105"
             )}>
-              <FontAwesomeIcon icon={item.icon} className="mr-2" />
-              <span className="text-primary-text-color">{item.label}</span>
-            </a>
-          </Link>
+            <FontAwesomeIcon icon={item.icon} className="mr-2" />
+            <span className="text-primary-text-color">{item.label}</span>
+          </Link>)
         ))}
       </div>
     </div>
