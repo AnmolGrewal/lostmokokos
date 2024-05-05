c363771f121aad342ba49ea79c07f78cccc8acad
anmolgrewal <anmolsgrewal@gmail.com>
2024-05-05 01:14:51 -0700

    Making sure full width and height is taken by body and html

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="size-full">
+      <body className={`size-full ${inter.className}`}>{children}</body>
     </html>
   );
 }
