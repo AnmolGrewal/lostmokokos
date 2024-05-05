11890f4657e04be008b0b9773e63fbbc931797ee
anmolgrewal <anmolsgrewal@gmail.com>
2024-05-05 04:53:10 -0700

    Adding more colors and styling scrollbar

--- tailwind-colors.js
+++ tailwind-colors.js
@@ -1,5 +1,6 @@
 module.exports = {
   'primary-background-color': '#161719',
+  'secondary-background-color': '#202226',
 
   'primary-text-color': '#ffffff',
   'primary-text-label-color': '#d7d7d9',
