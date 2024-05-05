9dfe4d628b241b7eebb9fa62188aae8a9b1a1ee4
anmolgrewal <anmolsgrewal@gmail.com>
2024-05-05 14:30:48 -0700

    Creating Default Raids page code

---
+++ src/pages/raids/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RaidsPage = () => {
+  return (
+    <div>
+      <h1>Raids</h1>
+      <p>Welcome to the Raids page. Content will be added here.</p>
+    </div>
+  );
+};
+
+export default RaidsPage;
