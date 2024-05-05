98215c6198d72e2e6859da485857c1fb5c2c60cb
anmolgrewal <anmolsgrewal@gmail.com>
2024-05-05 14:30:58 -0700

    Updating NavigationBar Sizing

--- src/app/components/NavigationBar.tsx
+++ src/app/components/NavigationBar.tsx
@@ -15,20 +15,20 @@ const NavigationBar = ({ currentPath }: NavigationBarProps) => {
 
   return (
     <div className="bg-secondary-background-color w-full h-12 flex justify-center items-center">
-      <div className="flex gap-4">
+      <div className="flex gap-4 items-center">
         {navItems.map(item => (
-          (<Link
-            href={item.path}
-            key={item.label}
-            passHref
-            className={clsx(
-              "flex items-center px-4 py-2 rounded-full transition-all duration-300 ease-in-out cursor-pointer",
+          <Link href={item.path} key={item.label} passHref>
+            <a className={clsx(
+              "flex items-center px-3 py-1 rounded-full transition-all duration-300 ease-in-out cursor-pointer",
               currentPath === item.path ? 'bg-blue-500' : 'bg-[#282B2F]',
               "hover:bg-blue-600 hover:scale-105"
             )}>
-            <FontAwesomeIcon icon={item.icon} className="mr-2" />
-            <span className="text-primary-text-color">{item.label}</span>
-          </Link>)
+              <div className="text-primary-text-color bg-[#393342] rounded-full p-2">
+                <FontAwesomeIcon icon={item.icon} className="text-xl" />
+              </div>
+              <span className="ml-2 text-lg text-primary-text-color">{item.label}</span>
+            </a>
+          </Link>
         ))}
       </div>
     </div>
