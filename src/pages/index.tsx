4f1918fa8da004b670d4b278e9f878daf5a6ca23
anmolgrewal <anmolsgrewal@gmail.com>
2024-05-05 20:12:42 -0700

    Navigate to raids for now

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,16 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/raids');
+  }, [router]);
+
   return (
     <main className="main-content bg-primary-background-color">
+      {/* You can add some loading animation or text here if needed */}
     </main>
   );
 }
