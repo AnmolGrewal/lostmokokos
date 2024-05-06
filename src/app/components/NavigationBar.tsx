5e333f4d54a2c55cb76ef0c75ce03641a5eb0e84
anmolgrewal <anmolsgrewal@gmail.com>
2024-05-05 20:13:50 -0700

    Updating Link

--- src/app/components/NavigationBar.tsx
+++ src/app/components/NavigationBar.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faDungeon } from '@fortawesome/free-solid-svg-icons';
 import clsx from 'clsx';
+import { useRouter } from 'next/router';
 
 interface NavigationBarProps {
   currentPath: string;
@@ -12,6 +13,8 @@ const NavigationBar = ({ currentPath }: NavigationBarProps) => {
     { path: "/", label: "Home", icon: faHome },
     { path: "/raids", label: "Raids", icon: faDungeon }
   ];
+
+  const router = useRouter();
 
   const isActive = (path: string) => {
     return path === "/" ? currentPath === path : currentPath.startsWith(path);
@@ -25,16 +28,26 @@ const NavigationBar = ({ currentPath }: NavigationBarProps) => {
             href={item.path}
             key={item.label}
             passHref
-            className={clsx(
-              "flex items-center px-3 py-1 rounded-full transition-all duration-300 ease-in-out cursor-pointer",
-              isActive(item.path) ? 'bg-primary-background-selection-color' : 'bg-chip-background-color',
-              !isActive(item.path) && "hover:bg-primary-background-hover-color hover:scale-105"
-            )}
           >
-            <div className="text-chip-text-color bg-image-background-color rounded-full p-2">
-              <FontAwesomeIcon icon={item.icon} className="text-xl" />
-            </div>
-            <span className="ml-2 text-lg text-chip-text-color font-bold">{item.label}</span>
+            <a
+              className={clsx(
+                "flex items-center px-3 py-1 rounded-full transition-all duration-300 ease-in-out",
+                isActive(item.path) ? 'bg-primary-background-selection-color cursor-not-allowed' : 'bg-chip-background-color',
+                !isActive(item.path) && "hover:bg-primary-background-hover-color hover:scale-105 cursor-pointer"
+              )}
+              onClick={(e) => {
+                if (isActive(item.path)) {
+                  e.preventDefault(); // Prevent link navigation if item is already selected
+                } else {
+                  router.push(item.path);
+                }
+              }}
+            >
+              <div className="text-chip-text-color bg-image-background-color rounded-full p-2">
+                <FontAwesomeIcon icon={item.icon} className="text-xl" />
+              </div>
+              <span className="ml-2 text-lg text-chip-text-color font-bold">{item.label}</span>
+            </a>
           </Link>
         ))}
       </div>
