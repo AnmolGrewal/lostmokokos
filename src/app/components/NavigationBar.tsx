b995c3df797fdc852d8fa8cdb4e2183e95332828
anmolgrewal <anmolsgrewal@gmail.com>
2024-05-05 20:14:47 -0700

    Removing click if already selected

--- src/app/components/NavigationBar.tsx
+++ src/app/components/NavigationBar.tsx
@@ -24,31 +24,28 @@ const NavigationBar = ({ currentPath }: NavigationBarProps) => {
     <div className="bg-secondary-background-color w-full h-14 flex justify-center items-center">
       <div className="flex gap-4 items-center">
         {navItems.map(item => (
-          <Link
+          (<Link
             href={item.path}
             key={item.label}
             passHref
-          >
-            <a
-              className={clsx(
-                "flex items-center px-3 py-1 rounded-full transition-all duration-300 ease-in-out",
-                isActive(item.path) ? 'bg-primary-background-selection-color cursor-not-allowed' : 'bg-chip-background-color',
-                !isActive(item.path) && "hover:bg-primary-background-hover-color hover:scale-105 cursor-pointer"
-              )}
-              onClick={(e) => {
-                if (isActive(item.path)) {
-                  e.preventDefault(); // Prevent link navigation if item is already selected
-                } else {
-                  router.push(item.path);
-                }
-              }}
-            >
-              <div className="text-chip-text-color bg-image-background-color rounded-full p-2">
-                <FontAwesomeIcon icon={item.icon} className="text-xl" />
-              </div>
-              <span className="ml-2 text-lg text-chip-text-color font-bold">{item.label}</span>
-            </a>
-          </Link>
+            className={clsx(
+              "flex items-center px-3 py-1 rounded-full transition-all duration-300 ease-in-out",
+              isActive(item.path) ? 'bg-primary-background-selection-color' : 'bg-chip-background-color',
+              !isActive(item.path) && "hover:bg-primary-background-hover-color hover:scale-105 cursor-pointer"
+            )}
+            onClick={(e) => {
+              if (isActive(item.path)) {
+                e.preventDefault(); // Prevent link navigation if item is already selected
+              } else {
+                router.push(item.path);
+              }
+            }}>
+            <div className="text-chip-text-color bg-image-background-color rounded-full p-2">
+              <FontAwesomeIcon icon={item.icon} className="text-xl" />
+            </div>
+            <span className="ml-2 text-lg text-chip-text-color font-bold">{item.label}</span>
+
+          </Link>)
         ))}
       </div>
     </div>
