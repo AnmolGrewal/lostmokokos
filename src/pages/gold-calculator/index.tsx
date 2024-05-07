2abce03cb1343a82333c902c7fb54f685a0e7872
anmolgrewal <anmolsgrewal@gmail.com>
2024-05-06 19:33:54 -0700

    Creating Gold Calcuator Page

---
+++ src/pages/gold-calculator/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GoldCalculatorIndexPage: React.FC = () => {
+
+  return (
+    <div className='bg-primary-background-color'>
+    </div>
+  );
+};
+
+export default GoldCalculatorIndexPage;
