e289adbc82869746f34661a9508cd795417eea59
anmolgrewal <anmolsgrewal@gmail.com>
2024-05-06 18:30:19 -0700

    Changing title to Lost Mokokos

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Lost Ark Helping Hand",
+  title: "Lost Mokokos",
   description: "Website to help players with their journey in Lost Ark",
 };
 
