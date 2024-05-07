af482d15cee5e9117171a0716b8ce2616ad64f84
anmolgrewal <anmolsgrewal@gmail.com>
2024-05-06 19:14:57 -0700

    Adding itemlevels

--- src/data/raidsInfo.ts
+++ src/data/raidsInfo.ts
@@ -1,6 +1,7 @@
 export interface GateData {
   gold: number[];
   boxCost: number[];
+  itemLevels: number[];
 }
 
 export interface Raid {
@@ -17,7 +18,8 @@ const raidsInfo: Raid[] = [
     imgSrc: "https://i.imgur.com/WcAVFsZ.png",
     gateData: {
       gold: [200, 300],
-      boxCost: [100, 150]
+      boxCost: [100, 150],
+      itemLevels: [1340],
     }
   },
   {
@@ -26,7 +28,8 @@ const raidsInfo: Raid[] = [
     imgSrc: "https://i.imgur.com/WcAVFsZ.png",
     gateData: {
       gold: [300, 400],
-      boxCost: [100, 150]
+      boxCost: [100, 150],
+      itemLevels: [1370],
     }
   },
   {
@@ -35,7 +38,8 @@ const raidsInfo: Raid[] = [
     imgSrc: "https://i.imgur.com/8sBbqnQ.png",
     gateData: {
       gold: [300, 300, 400],
-      boxCost: [100, 150, 150]
+      boxCost: [100, 150, 150],
+      itemLevels: [1370],
     }
   },
   {
@@ -44,7 +48,8 @@ const raidsInfo: Raid[] = [
     imgSrc: "https://i.imgur.com/ApCDeQb.png",
     gateData: {
       gold: [500, 700],
-      boxCost: [300, 400]
+      boxCost: [300, 400],
+      itemLevels: [1415],
     }
   },
   {
@@ -53,7 +58,8 @@ const raidsInfo: Raid[] = [
     imgSrc: "https://i.imgur.com/ApCDeQb.png",
     gateData: {
       gold: [700, 1100],
-      boxCost: [450, 600]
+      boxCost: [450, 600],
+      itemLevels: [1445],
     }
   },
   {
@@ -62,7 +68,8 @@ const raidsInfo: Raid[] = [
     imgSrc: "https://i.imgur.com/5VoXEOB.png",
     gateData: {
       gold: [600, 1000],
-      boxCost: [300, 450]
+      boxCost: [300, 450],
+      itemLevels: [1430],
     }
   },
   {
@@ -71,7 +78,8 @@ const raidsInfo: Raid[] = [
     imgSrc: "https://i.imgur.com/5VoXEOB.png",
     gateData: {
       gold: [900, 1500],
-      boxCost: [500, 650]
+      boxCost: [500, 650],
+      itemLevels: [1460],
     }
   },
   {
@@ -80,7 +88,8 @@ const raidsInfo: Raid[] = [
     imgSrc: "https://i.imgur.com/hOOSdDm.png",
     gateData: {
       gold: [600, 900, 1500],
-      boxCost: [300, 500, 700]
+      boxCost: [300, 500, 700],
+      itemLevels: [1475],
     }
   },
   {
@@ -89,7 +98,8 @@ const raidsInfo: Raid[] = [
     imgSrc: "https://i.imgur.com/bL9k49k.png",
     gateData: {
       gold: [2000, 2500, 1500, 2500],
-      boxCost: [400, 600, 800, 1500]
+      boxCost: [400, 600, 800, 1500],
+      itemLevels: [1490, 1490, 1500, 1520],
     }
   },
   {
@@ -98,7 +108,8 @@ const raidsInfo: Raid[] = [
     imgSrc: "https://i.imgur.com/bL9k49k.png",
     gateData: {
       gold: [2500, 3000, 2000, 3000],
-      boxCost: [700, 900, 1100, 1800]
+      boxCost: [700, 900, 1100, 1800],
+      itemLevels: [1540, 1540, 1550, 1560],
     }
   },
   {
@@ -107,7 +118,8 @@ const raidsInfo: Raid[] = [
     imgSrc: "https://i.imgur.com/2P9urFh.png",
     gateData: {
       gold: [1000, 1500, 2000],
-      boxCost: [600, 800, 1000]
+      boxCost: [600, 800, 1000],
+      itemLevels: [1540],
     }
   },
   {
@@ -116,7 +128,8 @@ const raidsInfo: Raid[] = [
     imgSrc: "https://i.imgur.com/2P9urFh.png",
     gateData: {
       gold: [1500, 2000, 3000],
-      boxCost: [700, 900, 1100]
+      boxCost: [700, 900, 1100],
+      itemLevels: [1580],
     }
   },
   {
@@ -125,7 +138,8 @@ const raidsInfo: Raid[] = [
     imgSrc: "https://i.imgur.com/W4ekupW.png",
     gateData: {
       gold: [1500, 2000, 4000],
-      boxCost: [900, 1100, 1500]
+      boxCost: [900, 1100, 1500],
+      itemLevels: [1580],
     }
   },
   {
@@ -134,7 +148,8 @@ const raidsInfo: Raid[] = [
     imgSrc: "https://i.imgur.com/W4ekupW.png",
     gateData: {
       gold: [1750, 2500, 5750],
-      boxCost: [1200, 1400, 1900]
+      boxCost: [1200, 1400, 1900],
+      itemLevels: [1600],
     }
   },
   {
@@ -143,7 +158,8 @@ const raidsInfo: Raid[] = [
     imgSrc: "https://i.imgur.com/sSdCEIA.png",
     gateData: {
       gold: [1500, 1750, 2500, 3250],
-      boxCost: [700, 800, 900, 1100]
+      boxCost: [700, 800, 900, 1100],
+      itemLevels: [1600],
     }
   },
   {
@@ -152,7 +168,8 @@ const raidsInfo: Raid[] = [
     imgSrc: "https://i.imgur.com/sSdCEIA.png",
     gateData: {
       gold: [2000, 2500, 4000, 6000],
-      boxCost: [1000, 1000, 1500, 2000]
+      boxCost: [1000, 1000, 1500, 2000],
+      itemLevels: [1620],
     }
   },
   {
@@ -161,7 +178,8 @@ const raidsInfo: Raid[] = [
     imgSrc: "https://i.imgur.com/464OcZx.png",
     gateData: {
       gold: [3500, 4000, 5500],
-      boxCost: [1500, 1800, 2500]
+      boxCost: [1500, 1800, 2500],
+      itemLevels: [1610],
     }
   },
   {
@@ -170,7 +188,8 @@ const raidsInfo: Raid[] = [
     imgSrc: "https://i.imgur.com/464OcZx.png",
     gateData: {
       gold: [5000, 6000, 9000, 21000],
-      boxCost: [2000, 2400, 2800, 3600]
+      boxCost: [2000, 2400, 2800, 3600],
+      itemLevels: [1630],
     }
   },
   {
@@ -179,7 +198,8 @@ const raidsInfo: Raid[] = [
     imgSrc: "https://i.imgur.com/tju1uI1.png",
     gateData: {
       gold: [5000, 9500],
-      boxCost: [2200, 3400]
+      boxCost: [2200, 3400],
+      itemLevels: [1620],
     }
   },
   {
@@ -188,7 +208,8 @@ const raidsInfo: Raid[] = [
     imgSrc: "https://i.imgur.com/tju1uI1.png",
     gateData: {
       gold: [6000, 12500],
-      boxCost: [2800, 4100]
+      boxCost: [2800, 4100],
+      itemLevels: [1630],
     }
   },
 ];
