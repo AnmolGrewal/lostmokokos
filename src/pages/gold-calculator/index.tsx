07a5cfb6abd72e1598d9180133b2bf0c7ea8caeb
anmolgrewal <anmolsgrewal@gmail.com>
2024-05-07 14:47:47 -0700

    Adding Gold Calculator Title

--- src/pages/gold-calculator/index.tsx
+++ src/pages/gold-calculator/index.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Helmet } from 'react-helmet';
 import GoldGrid from '../../app/components/GoldGrid';
 import raidsInfo from '../../data/raidsInfo';
 
 const GoldCalculatorIndexPage: React.FC = () => {
+  useEffect(() => {
+    document.title = 'Gold Calculator';
+  }, []);
 
   return (
     <div className='bg-primary-background-color'>
+      <Helmet>
+        <title>Gold Calculator</title>
+        <meta name="description" content="Description of Gold Calculator" />
+      </Helmet>
       <GoldGrid raids={raidsInfo} />
     </div>
   );
