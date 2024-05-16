cfe3408f55a0c3d4eb47d855c557b4a7e1ef1e30
anmolgrewal <anmolsgrewal@gmail.com>
2024-05-15 22:40:14 -0700

    Adding Honor Shards Data

---
+++ src/data/imageLinks.ts
@@ -0,0 +1,10 @@
+export interface ImageData {
+  honorShards: string;
+}
+
+const imagesData: ImageData = 
+  {
+    honorShards: "https://i.imgur.com/3Gzm5YD.png"
+  };
+
+export default imagesData;
