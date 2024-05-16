e920cc92b2b7211591e445f138c2142623cd7a0c
anmolgrewal <anmolsgrewal@gmail.com>
2024-05-15 23:52:36 -0700

    Adding Chaos Stones Data and IMG

--- src/data/imageLinks.ts
+++ src/data/imageLinks.ts
@@ -1,10 +1,12 @@
 export interface ImageData {
   honorShards: string;
+  chaosStones: string;
 }
 
 const imagesData: ImageData = 
   {
-    honorShards: "https://i.imgur.com/3Gzm5YD.png"
+    honorShards: "https://i.imgur.com/3Gzm5YD.png",
+    chaosStones: "https://i.imgur.com/pcUJEZq.png"
   };
 
 export default imagesData;
--- src/data/raidsInfo.ts
+++ src/data/raidsInfo.ts
@@ -5,6 +5,7 @@ export interface GateData {
   gateRewards?: number[][];
   honorShards?: number[];
   boxHonorShards?: number[];
+  chaosStones?: number[];
 }
 
 export interface Raid {
@@ -66,6 +67,7 @@ const raidsInfo: Raid[] = [
       gateRewards: [[1, 3], [2, 3]],
       honorShards: [400, 600],
       boxHonorShards: [400, 700],
+      chaosStones: [0, 1],
     }
   },
   {
@@ -81,6 +83,7 @@ const raidsInfo: Raid[] = [
       gateRewards: [[3], [3]],
       honorShards: [500, 700],
       boxHonorShards: [1000, 1300],
+      chaosStones: [0, 1],
     }
   },
   {
@@ -96,6 +99,7 @@ const raidsInfo: Raid[] = [
       gateRewards: [[2, 1], [4, 2]],
       honorShards: [500, 700],
       boxHonorShards: [700, 1000],
+      chaosStones: [0, 1],
     }
   },
   {
@@ -111,6 +115,7 @@ const raidsInfo: Raid[] = [
       gateRewards: [[3], [3]],
       honorShards: [600, 900],
       boxHonorShards: [1600, 2000],
+      chaosStones: [0, 1],
     }
   },
   {
@@ -126,6 +131,7 @@ const raidsInfo: Raid[] = [
       gateRewards: [[1], [2], [2]],
       honorShards: [800, 800, 800],
       boxHonorShards: [1300, 1300, 1600],
+      chaosStones: [0, 0, 1],
     }
   },
   {
@@ -141,6 +147,7 @@ const raidsInfo: Raid[] = [
       gateRewards: [[4], [4], [5], [7]],
       honorShards: [1000, 1200, 1500, 3000],
       boxHonorShards: [2100, 2700, 3400, 7000],
+      chaosStones: [0, 0, 1, 3],
     }
   },
   {
@@ -156,6 +163,7 @@ const raidsInfo: Raid[] = [
       gateRewards: [[6], [6], [7], [10]],
       honorShards: [2500, 2500, 3000, 6000],
       boxHonorShards: [3000, 4000, 5200, 10000],
+      chaosStones: [0, 0, 2, 5],
     }
   },
   {
@@ -171,6 +179,7 @@ const raidsInfo: Raid[] = [
       gateRewards: [[11], [12,1], [17,2]],
       honorShards: [2000, 2500, 4000],
       boxHonorShards: [1000, 1500, 2000],
+      chaosStones: [0, 0, 4],
     }
   },
   {
@@ -186,6 +195,7 @@ const raidsInfo: Raid[] = [
       gateRewards: [[14,1], [16,1], [20,3]],
       honorShards: [2500, 3500, 5000],
       boxHonorShards: [1500, 2000, 2500],
+      chaosStones: [0, 0, 3],
     }
   },
   {
@@ -201,6 +211,7 @@ const raidsInfo: Raid[] = [
       gateRewards: [[3], [3], [5]],
       honorShards: [1600, 1600, 2400],
       boxHonorShards: [3000, 3000, 4200],
+      chaosStones: [0, 0, 3],
     }
   },
   {
@@ -216,6 +227,7 @@ const raidsInfo: Raid[] = [
       gateRewards: [[7], [7], [8]],
       honorShards: [2400, 2400, 4000],
       boxHonorShards: [4000, 4000, 5500],
+      chaosStones: [0, 0, 3],
     }
   },
   {
@@ -231,6 +243,7 @@ const raidsInfo: Raid[] = [
       gateRewards: [[2], [2], [3], [1,1]],
       honorShards: [1500, 1500, 2000, 2000],
       boxHonorShards: [3000, 3000, 4000, 4500],
+      chaosStones: [0, 1, 0, 2],
     }
   },
   {
@@ -246,6 +259,7 @@ const raidsInfo: Raid[] = [
       gateRewards: [[2], [2], [3], [1,1]],
       honorShards: [1800, 1800, 2400, 2400],
       boxHonorShards: [4000, 4000, 5000, 5500],
+      chaosStones: [0, 1, 0, 2],
     }
   },
   {
@@ -261,6 +275,7 @@ const raidsInfo: Raid[] = [
       gateRewards: [[2,3], [3,4], [4,6]],
       honorShards: [2000, 2500, 3000],
       boxHonorShards: [3500, 4500, 6000],
+      chaosStones: [0, 0, 5],
     }
   },
   {
@@ -276,6 +291,7 @@ const raidsInfo: Raid[] = [
       gateRewards: [[6,6], [9,8], [12,12], [12,12]],
       honorShards: [2400, 3000, 3600, 4500],
       boxHonorShards: [5000, 6000, 7500, 9000],
+      chaosStones: [0, 0, 7, 10],
     }
   },
   {
