e73e96ba9f3e2e728975a4248b7c440cf7f34242
anmolgrewal <anmolsgrewal@gmail.com>
2024-05-20 17:03:02 -0700

    Updating destructionStones Link

--- src/data/imageLinks.ts
+++ src/data/imageLinks.ts
@@ -1,11 +1,13 @@
 export interface ImageData {
   honorShards: string;
   chaosStones: string;
+  destructionStones: string;
 }
 
 const imagesData: ImageData = {
   honorShards: 'https://i.imgur.com/3Gzm5YD.png',
   chaosStones: 'https://i.imgur.com/pcUJEZq.png',
+  destructionStones: 'https://i.imgur.com/TZHKul7.png',
 };
 
 export default imagesData;
