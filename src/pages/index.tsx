79e9d41b46e5bb2c86ba286fd769a974149711fb
anmolgrewal <anmolsgrewal@gmail.com>
2024-05-29 17:12:03 -0700

    Updated index to not take TimeLine anymore and instead take changelog component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { Helmet } from 'react-helmet';
-import Timeline from '@mui/lab/Timeline';
-import TimelineItem from '@mui/lab/TimelineItem';
-import TimelineSeparator from '@mui/lab/TimelineSeparator';
-import TimelineConnector from '@mui/lab/TimelineConnector';
-import TimelineContent from '@mui/lab/TimelineContent';
-import TimelineDot from '@mui/lab/TimelineDot';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
 import emailjs from 'emailjs-com';
+import ChangeLog from '../app/components/ChangeLog';
 
 export default function Home() {
   const [todos, setTodos] = useState<string[]>([]);
@@ -155,54 +148,7 @@ export default function Home() {
         </form>
       </div>
       <div className="m-5 bg-secondary-background-color p-5 rounded-lg">
-        <hr className="my-8" />
-        <Timeline position="alternate-reverse">
-          {[
-            'Updated Manage Raids styling to work properly for Active, Inactive and Hover over Raid Chips and Help Text',
-            'Added Basic HomePage for Updates and Communication',
-            'Todo List is only temporary for fun will probably remove it later or revamp it',
-            'Updated Total Gold Row in Gold Calculator to Be Visible Always on the Bottom, made it mobile friendly',
-            'Removing Page Refresh when you add something to Todo List',
-            'Updated Communication Text to be More User Friendly with a Timeline Instead of a List',
-            'Working on Thaemine and Echnida Removal Fix, Most Likely rewriting The Raid Calculator from scratch to be more efficient and easier to maintain',
-            'Added a New Row called Additional Gold to Add your own Gold to the Total Gold Calculation based on each Individual Character',
-            "Fixed Issue with text cutting into Checkbox's for labels on Smaller Windows",
-            'Adding Show Differences Icon on Raids instead of doing mouse hover to be more mobile friendly',
-            'Updating Gold Formatting to your currency locale formatting',
-            'Lined up Additional Gold with Total Gold for better readability',
-            'Updated Raids Data to Include Boss Drops Upto Echnida',
-            'Working on fixing totals for double boss rewards stacking on each other in raids Page',
-            'Added Trash Can Icon to Clear All Data from Gold Calculator Except Raid Selections',
-            'Managing Raids is now 2 columns if you have a smaller screen or width',
-            'Added All Honor Shard Rewards Upto Thaemine!',
-            "Added a Popup to confirm to clear data so you don't accidently delete it :)",
-            'Adding Chaos Stones to Raid Grid',
-            'Fixed bug that Additional Gold was Adding Twice to Total Gold',
-            'Rewrote Gold Calculator Special Key to Calculate Gold for Each Raid',
-            'Fixed Thaemine and Echnida Raid Removal Issues for Gold Calculator!',
-            'Adding Destruction Stones to Raid Grid, Boxs and Clears',
-            'Adding Contact Me Support for Requests and Bug Reporting!',
-            'Fixed Title Tab Issues for Hard Mode for Raids Page',
-            'Boss Rewards for Box and Clear are the same so they are not added twice',
-            'Fixed Removing Thaemine or Echnida Raids! If You are Having Issues please click Trash Can to Delete Data and Reset!',
-            'Added Raid Info for Boss Drops, Chaos Stones, Destruction Stones and tooltips.',
-            'Added Daily and Weekly Todo!!!! Chaos Gates, Una Tasks, Guardian Raids and Guild Weeklies! That automatically resets daily and weekly!',
-          ]
-            .reverse()
-            .map((content, index) => (
-              <TimelineItem key={index}>
-                <TimelineSeparator>
-                  <TimelineDot />
-                  <TimelineConnector />
-                </TimelineSeparator>
-                <TimelineContent>
-                  <Paper elevation={3} className="p-3 bg-primary-background-color text-primary-text-color">
-                    <Typography>{content}</Typography>
-                  </Paper>
-                </TimelineContent>
-              </TimelineItem>
-            ))}
-        </Timeline>
+        <ChangeLog />
       </div>
     </main>
   );
