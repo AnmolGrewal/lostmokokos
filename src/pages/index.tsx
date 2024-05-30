7938f23c0d6c66cda053e931d9f95da052d57aa3
anmolgrewal <anmolsgrewal@gmail.com>
2024-05-29 19:25:53 -0700

    Removing emailjs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { Helmet } from 'react-helmet';
-import emailjs from 'emailjs-com';
 import ChangeLog from '../app/components/ChangeLog';
 
 export default function Home() {
@@ -37,29 +36,6 @@ export default function Home() {
   const handleRemoveTodo = (index: number) => {
     const newTodos = todos.filter((_, i) => i !== index);
     setTodos(newTodos);
-  };
-
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: '',
-  });
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    emailjs.send('service_xg7o1zp', 'template_5solus9', formData, 'etp9mxdwYiseBfjZj')
-      .then((response) => {
-        console.log('Email sent successfully!', response.status, response.text);
-        setFormData({ name: '', email: '', message: '' });
-      })
-      .catch((error) => {
-        console.error('Error sending email:', error);
-      });
   };
 
   return (
@@ -108,46 +84,6 @@ export default function Home() {
       </div>
       <div className="m-5 bg-secondary-background-color p-5 rounded-lg">
         <h2 className="text-lg text-primary-text-color mb-4">Contact Me</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label htmlFor="name" className="block mb-2">Name:</label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-              className="w-full p-2 bg-primary-background-color text-white rounded"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="email" className="block mb-2">Email:</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              className="w-full p-2 bg-primary-background-color text-white rounded"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="message" className="block mb-2">Message:</label>
-            <textarea
-              id="message"
-              name="message"
-              value={formData.message}
-              onChange={handleChange}
-              className="w-full p-2 bg-primary-background-color text-white rounded"
-              required
-            ></textarea>
-          </div>
-          <button type="submit" className="bg-primary-background-selection-color text-white px-4 py-2 rounded">
-            Send
-          </button>
-        </form>
       </div>
       <div className="m-5 bg-secondary-background-color p-5 rounded-lg">
         <ChangeLog />
