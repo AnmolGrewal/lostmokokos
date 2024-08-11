2cee04da3d4d16b7671ff91850bba097ad2d9168
anmolgrewal <anmolsgrewal@gmail.com>
2024-08-10 19:59:01 -0700

    Adding Chaos Dungeon Data

---
+++ src/data/chaosInfo.ts
@@ -0,0 +1,123 @@
+export interface ChaosData {
+  label: string;
+  itemLevel: number;
+  destructionStone: number;
+  guardiannStone: number;
+  leapstone: number;
+  gem: number;
+  honorShard: number;
+  silver: number;
+}
+
+const chaosInfo: ChaosData[] = [
+  {
+    label: 'South Vern',
+    itemLevel: 1415,
+    destructionStone: 75,
+    guardiannStone: 223,
+    leapstone: 5,
+    gem: 0.7,
+    honorShard: 2373,
+    silver: 69270,
+  },
+  {
+    label: 'South Vern',
+    itemLevel: 1445,
+    destructionStone: 82,
+    guardiannStone: 236,
+    leapstone: 6,
+    gem: 0.8,
+    honorShard: 2541,
+    silver: 69656,
+  },
+  {
+    label: 'South Vern',
+    itemLevel: 1475,
+    destructionStone: 88,
+    guardiannStone: 260,
+    leapstone: 6,
+    gem: 0.9,
+    honorShard: 2815,
+    silver: 71378,
+  },
+  {
+    label: 'South Vern',
+    itemLevel: 1490,
+    destructionStone: 50,
+    guardiannStone: 149,
+    leapstone: 4,
+    gem: 0.9,
+    honorShard: 5209,
+    silver: 74117,
+  },
+  {
+    label: 'South Vern',
+    itemLevel: 1520,
+    destructionStone: 60,
+    guardiannStone: 176,
+    leapstone: 4,
+    gem: 1.2,
+    honorShard: 6751,
+    silver: 73599,
+  },
+  {
+    label: 'South Vern',
+    itemLevel: 1540,
+    destructionStone: 68,
+    guardiannStone: 199,
+    leapstone: 5,
+    gem: 1.2,
+    honorShard: 7942,
+    silver: 77851,
+  },
+  {
+    label: 'South Vern',
+    itemLevel: 1560,
+    destructionStone: 78,
+    guardiannStone: 237,
+    leapstone: 6,
+    gem: 1.1,
+    honorShard: 9683,
+    silver: 77951,
+  },
+  {
+    label: 'Elgacia',
+    itemLevel: 1580,
+    destructionStone: 40,
+    guardiannStone: 108,
+    leapstone: 3,
+    gem: 1.2,
+    honorShard: 9914,
+    silver: 77091,
+  },
+  {
+    label: 'Elgacia',
+    itemLevel: 1600,
+    destructionStone: 43,
+    guardiannStone: 123,
+    leapstone: 4,
+    gem: 1.2,
+    honorShard: 9678,
+    silver: 78908,
+  },
+  {
+    label: 'Voldis',
+    itemLevel: 1610,
+    destructionStone: 58,
+    guardiannStone: 167,
+    leapstone: 5,
+    gem: 1.1,
+    honorShard: 10677,
+    silver: 91704,
+  },
+  {
+    label: 'Voldis',
+    itemLevel: 1630,
+    destructionStone: 96,
+    guardiannStone: 275,
+    leapstone: 9,
+    gem: 1.4,
+    honorShard: 14515,
+    silver: 97820,
+  },
+];
