cd2ea6d7bb8c43063b78b9968f305849eb5c4f11
anmolgrewal <anmolsgrewal@gmail.com>
2024-08-10 20:02:33 -0700

    Adding Guardian Data

---
+++ src/data/guardiansInfo.ts
@@ -0,0 +1,59 @@
+export interface GuardianData {
+  label: string;
+  itemLevel: number;
+  destructionStone: number;
+  guardiannStone: number;
+  leapstone: number;
+}
+
+const guardianInfo: GuardianData[] = [
+  {
+    label: 'Deskaluda',
+    itemLevel: 1415,
+    destructionStone: 101,
+    guardiannStone: 315,
+    leapstone: 11,
+  },
+  {
+    label: 'Kungelanium',
+    itemLevel: 1460,
+    destructionStone: 133,
+    guardiannStone: 408,
+    leapstone: 16,
+  },
+  {
+    label: 'Caliligos',
+    itemLevel: 1490,
+    destructionStone: 75,
+    guardiannStone: 226,
+    leapstone: 10,
+  },
+  {
+    label: 'Hanumatan',
+    itemLevel: 1540,
+    destructionStone: 101,
+    guardiannStone: 306,
+    leapstone: 14,
+  },
+  {
+    label: 'Sonavel',
+    itemLevel: 1580,
+    destructionStone: 68,
+    guardiannStone: 204,
+    leapstone: 8,
+  },
+  {
+    label: 'Gargadeth',
+    itemLevel: 1610,
+    destructionStone: 103,
+    guardiannStone: 301,
+    leapstone: 12,
+  },
+  {
+    label: 'Vescal',
+    itemLevel: 1630,
+    destructionStone: 166,
+    guardiannStone: 506,
+    leapstone: 24,
+  },
+];
